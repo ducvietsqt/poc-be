0768e26bb9431cb52dea16f4895c5d24f66a90bc
Thang Diep <diepvuongthang@gmail.com>
2021-09-16 15:21:31 +0700

    seed data

--- app/model/seed/spin.js
+++ app/model/seed/spin.js
@@ -1,11 +1,12 @@
 const Model = require("app/model").spins;
+const config = require("app/config");
 
 module.exports = async () => {
   let count = await Model.count();
   if (count == 0) {
     await Model.create({
       number: 0,
-      secret: 4
+      secret: config.insight.secret
     }, {
       returning: true
     });
