a073d591ea43574d73154934fb13dacd7743c224
hoducviet <ducvietsqt@gmail.com>
2021-11-01 20:11:15 +0700

    fix spellcheck

--- server.js
+++ server.js
@@ -26,55 +26,54 @@ database.init(async err => {
     logger.info('Resync data model and do not drop any data');
     require('app/model/seed');
   });
+});
+app.set('trust proxy', 1);
+app.use('/', require('app/index'));
+app.use(express.static('public'));
+const server = http.createServer(app);
+server.listen(process.env.PORT, async function () {
 
-  app.set('trust proxy', 1);
-  app.use('/', require('app/index'));
-  app.use(express.static('public'));
-  const server = http.createServer(app);
-  server.listen(process.env.PORT, async function () {
-    console.log(`server start successfully on port: ${process.env.PORT}`);
-    var exec = require('child_process').exec;
-    var cmd = 'npx sequelize-cli db:migrate';
-    exec(cmd, function (error, stdout, stderr) {
-      console.log('error', error);
-      console.log('stdout ', stdout);
-      console.log('stderr ', stderr);
+  console.log(`server start successfully on port: ${process.env.PORT}`);
+  var exec = require('child_process').exec;
+  var cmd = 'npx sequelize-cli db:migrate';
+  exec(cmd, function (error, stdout, stderr) {
+    console.log('error', error);
+    console.log('stdout ', stdout);
+    console.log('stderr ', stderr);
+  })
+  var current = await Insight.getCurrentSpin();
+  console.log('current', current);
+  var spin = await Insight.getSpin(current);
+  console.log('spin', spin);
+  if(!spin || spin.bankHash == '0x0000000000000000000000000000000000000000000000000000000000000000') {
+    let tx = await Insight.setInitialBankHash(config.insight.secret);
+    console.log('tx: ', tx);
+    let spin = await Spin.findOne({
+      where: {
+        number: parseInt(current)
+      }
     })
-    var current = await Insight.getCurrentSpin();
-    console.log('current', current);
-    var spin = await Insight.getSpin(current);
-    console.log('spin', spin);
-    if(!spin || spin.bankHash == '0x0000000000000000000000000000000000000000000000000000000000000000') {
-      let tx = await Insight.setInitialBankHash(config.insight.secret);
-      console.log('tx: ', tx);
-      let spin = await Spin.findOne({
+    if (spin) {
+      await Spin.update({
+        secret: config.insight.secret,
+        start_tx_hash: tx
+      }, {
         where: {
-          number: parseInt(current)
+          number: current
         }
       })
-      if (spin) {
-        await Spin.update({
-          secret: config.insight.secret,
-          start_tx_hash: tx
-        }, {
-          where: {
-            number: current
-          }
-        })
-      } else {
-        await Spin.create({
-          number: parseInt(current),
-          secret: config.insight.secret,
-          start_tx_hash: tx
-        })
-      }
+    } else {
+      await Spin.create({
+        number: parseInt(current),
+        secret: config.insight.secret,
+        start_tx_hash: tx
+      })
     }
-  });
-  process.on('SIGINT', () => {
-    process.exit(0);
-  });
+  }
 });
-
+process.on('SIGINT', () => {
+  process.exit(0);
+});
 process.on('unhandledRejection', function (reason, p) {
   logger.error('unhandledRejection', reason, p);
 });
